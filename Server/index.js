8d8e9c7737ce8b88903a9ca24d6210d29ab26a6a
Kejuan Harrison <bamaboi912@icloud.com>
2023-02-23 13:21:32 -0600

    created index.js

---
+++ Server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', true);
+
+//set up middle ware
+app.use(express.json());
+const cors = require("cors");
+
+app.use(cors());
+
+//establish communication to MongoDB atlas (database)
+
+mongoose.connect("mongodb+srv://bamaboi912:Student1234!@cluster0.lpofvla.mongodb.net/ExoticDealership?retryWrites=true&w=majority").then(() => 
+console.log("Connected to Database")).then(() =>{
+    app.listen(3001)
+}).catch((err) => console.log(err));
